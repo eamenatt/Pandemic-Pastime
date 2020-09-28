cff966fa11439c22a3ede1d5acdbcb5298b604dd
Eric <erw88@yahoo.com>
2020-09-28 19:45:12 -0400

    added functionality

--- scripts.js
+++ scripts.js
@@ -28,14 +28,14 @@ $("document").ready(function () {
     });
 
     //store values of ingredients in Array
-    var ingredientsArray = [];
-    $(".button").click(function () {
-        var list = document.getElementById('ingredientList').childNodes;
-        for (var i = 0; i < list.length; i++) {
-            var arrValue = list[i].innerHTML;
-            ingredientsArray.push(arrValue);
-        }
-    });
+    
+    //$(".button").click(function () {
+        //var list = document.getElementById('ingredientList').childNodes;
+        //for (var i = 0; i < list.length; i++) {
+            //var arrValue = list[i].innerHTML;
+            //ingredientsArray.push(arrValue);
+        //}
+    //});
 
 
     //var listItems = document.getElementById('ingredientList').getElementsByTagName('li');
@@ -45,11 +45,45 @@ $("document").ready(function () {
     $("#recipe-query").on("click", function () {
         console.log("Recipe search button has been clicked");
 
-        for (var i = 0; i < ingredientsArray.length; i++) {
-            ingredients += ingredientsArray[i];
-            //return ingredients;
+        var list = document.getElementById('ingredientList').childNodes;
+        for (var i = 0; i < list.length; i++) {
+            var arrValue = list[i].innerHTML;
+            ingredientsArray.push(arrValue);
+            ingredients += "," + arrValue;
+        }
 
-        }; 
+        
+        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients + "&ignorePantry=" + ignorePantry + "&instructionsRequired=" + includeInstructions + "&number=" + number;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log(response[index].image);
+            console.log(response[index].title);
+            console.log(response[index].id);
+
+            displayImage = response[index].image;
+            displayTitle = response[index].title;
+            //code to append to webpage
+
+            //This will get recipe instructions by the recipe ID
+            var recipeID = response[index].id;
+            var queryURL2 = "https://api.spoonacular.com/recipes/" + recipeID + "/information?apiKey=" + apiKey;
+
+            $.ajax({
+                url: queryURL2,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                console.log(response.instructions);
+
+                displayInstructions = response.instructions;
+                //code to append to webpage
+            });
+        });
+
         console.log(ingredients);
         console.log(ingredientsArray);
         $("#recipe-display").removeClass("hidden");
@@ -75,57 +109,48 @@ $("document").ready(function () {
 
     //DISPLAY VARIABLES
     //Displays recipe instructiosn on page
-    var displayInstructions = "";
-    var displayTitle = "";
-    var displayImage = "";
+    //var displayInstructions = "";
+    //var displayTitle = "";
+    //var displayImage = "";
 
     //Add an onclick "Next Recipe" to +1 the index
     var index = 0;
     document.getElementById("NextBtn").onclick = function () {
         index++;
+
     }
 
     //Find recipe by ingredients
     var apiKey = "9c7455831c634e4c8e907857c9b77b2c"
-    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients + "&ignorePantry=" + ignorePantry + "&instructionsRequired=" + includeInstructions + "&number=" + number;
+    // var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients + "&ignorePantry=" + ignorePantry + "&instructionsRequired=" + includeInstructions + "&number=" + number;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        console.log(response[index].image);
-        console.log(response[index].title);
-        console.log(response[index].id);
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function (response) {
+    //     console.log(response);
+    //     console.log(response[index].image);
+    //     console.log(response[index].title);
+    //     console.log(response[index].id);
 
-        displayImage = response[index].image;
-        displayTitle = response[index].title;
-        //code to append to webpage
+    //     displayImage = response[index].image;
+    //     displayTitle = response[index].title;
+    //     //code to append to webpage
 
-        //This will get recipe instructions by the recipe ID
-        var recipeID = response[index].id;
-        var queryURL2 = "https://api.spoonacular.com/recipes/" + recipeID + "/information?apiKey=" + apiKey;
+    //     //This will get recipe instructions by the recipe ID
+    //     var recipeID = response[index].id;
+    //     var queryURL2 = "https://api.spoonacular.com/recipes/" + recipeID + "/information?apiKey=" + apiKey;
 
-        $.ajax({
-            url: queryURL2,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.instructions);
+    //     $.ajax({
+    //         url: queryURL2,
+    //         method: "GET"
+    //     }).then(function (response) {
+    //         console.log(response);
+    //         console.log(response.instructions);
 
-            displayInstructions = response.instructions;
-            //code to append to webpage
-        });
-    });
-
-});
-
-$("#wine-query").on("click", function () {
-    console.log("wine pairing button has been clicked");
-
-    $("#wine-display").removeClass("hidden");
-    $("#search-options").addClass("hidden");
+    //         displayInstructions = response.instructions;
+    //         //code to append to webpage
+    //     });
+    // });
 
 });
-
-
