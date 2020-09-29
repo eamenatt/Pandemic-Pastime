67e5a73e0a08d276c95c4edbd803af31e52b96c1
Eric <erw88@yahoo.com>
2020-09-29 02:56:38 -0400

    added some functionality to recipes pace

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 $("document").ready(function () {
-    //store values of ingredients in Array
+
     var ingredientsArray = [];
     var recipeArray = [];
+    var apiKey = "9c7455831c634e4c8e907857c9b77b2c";
+
+    //PARAMETERS
+    //The maximum number of recipes to return (between 1 and 100). Defaults to 10
+    var number = 5;
+    //Whether to ignore typical pantry items, such as water, salt, flour, etc
+    var ignorePantry = true;
+    //Includes instructions in JSON object
+    var includeInstructions = true;
 
     document.getElementById("addIngrBtn").onclick = function () {
 
@@ -27,19 +36,6 @@ $("document").ready(function () {
         return false;
     });
 
-    //store values of ingredients in Array
-    
-    //$(".button").click(function () {
-        //var list = document.getElementById('ingredientList').childNodes;
-        //for (var i = 0; i < list.length; i++) {
-            //var arrValue = list[i].innerHTML;
-            //ingredientsArray.push(arrValue);
-        //}
-    //});
-
-
-    //var listItems = document.getElementById('ingredientList').getElementsByTagName('li');
-
 
     var ingredients = "";
     $("#recipe-query").on("click", function () {
@@ -52,7 +48,7 @@ $("document").ready(function () {
             ingredients += "," + arrValue;
         }
 
-        
+
         var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients + "&ignorePantry=" + ignorePantry + "&instructionsRequired=" + includeInstructions + "&number=" + number;
 
         $.ajax({
@@ -63,12 +59,10 @@ $("document").ready(function () {
             console.log(response[index].image);
             console.log(response[index].title);
             console.log(response[index].id);
+            console.log(response[index].missedIngredients[0].name);
             recipeArray = response;
 
-            //displayImage = response[index].image;
-            //displayTitle = response[index].title;
-            //code to append to webpage
-            //$("#apiTitle").text(displayTitle);
+            //function will append to webpage
             renderRecipe();
 
             //This will get recipe instructions by the recipe ID
@@ -82,9 +76,10 @@ $("document").ready(function () {
                 console.log(response);
                 console.log(response.instructions);
 
-                displayInstructions = response.instructions;
-                //code to append to webpage
+                $("#instruct").text(response.instructions);
+                $("#time").text(response.preparationMinutes + " minutes");
             });
+
         });
 
         console.log(ingredients);
@@ -94,32 +89,14 @@ $("document").ready(function () {
 
     });
 
-    function renderRecipe(){
+    //Displays API info to page
+    function renderRecipe() {
         $("#apiTitle").text(recipeArray[index].title);
+        $(".recipe-img").attr("src", recipeArray[index].image);
     }
-    //A comma-separated list of ingredients that the recipes should contain
-    //var ingredients = "";
-    //for (var i = 0; i < ingredientsArray.length; i++) {
-    //    ingredients += ingredientsArray[i];
-    //    return ingredients;
-    //};
 
-    //The maximum number of recipes to return (between 1 and 100). Defaults to 10
-    var number = 5;
 
-    //Whether to ignore typical pantry items, such as water, salt, flour, etc
-    var ignorePantry = true;
-
-    //Includes instructions in JSON object
-    var includeInstructions = true;
-
-    //DISPLAY VARIABLES
-    //Displays recipe instructiosn on page
-    //var displayInstructions = "";
-    //var displayTitle = "";
-    //var displayImage = "";
-
-    //Add an onclick "Next Recipe" to +1 the index
+    //onclick "Next Recipe" to +1 the index
     var index = 0;
     document.getElementById("NextBtn").onclick = function () {
         index++;
@@ -127,37 +104,5 @@ $("document").ready(function () {
 
     }
 
-    //Find recipe by ingredients
-    var apiKey = "9c7455831c634e4c8e907857c9b77b2c"
-    // var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients + "&ignorePantry=" + ignorePantry + "&instructionsRequired=" + includeInstructions + "&number=" + number;
-
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(response);
-    //     console.log(response[index].image);
-    //     console.log(response[index].title);
-    //     console.log(response[index].id);
-
-    //     displayImage = response[index].image;
-    //     displayTitle = response[index].title;
-    //     //code to append to webpage
-
-    //     //This will get recipe instructions by the recipe ID
-    //     var recipeID = response[index].id;
-    //     var queryURL2 = "https://api.spoonacular.com/recipes/" + recipeID + "/information?apiKey=" + apiKey;
-
-    //     $.ajax({
-    //         url: queryURL2,
-    //         method: "GET"
-    //     }).then(function (response) {
-    //         console.log(response);
-    //         console.log(response.instructions);
-
-    //         displayInstructions = response.instructions;
-    //         //code to append to webpage
-    //     });
-    // });
 
 });
