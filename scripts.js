8acb91c86ac138b4ffaa9142bb117b6e75dc4027
Eric M <eamenatt@gmail.com>
2020-09-29 20:26:31 -0400

    updated api key

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $("document").ready(function () {
     var ingredientsArray = [];
     var recipeArray = [];
 
-    var apiKey = "78d70e51419243b7b009c0a352741717";
+    var apiKey = "41e877c90d244ab6a5620b9a76f2e764";
 
     //PARAMETERS
     //The maximum number of recipes to return (between 1 and 100). Defaults to 10
